1935a76bbb2c57681922d9c56f40d447d07252e1
Romulo Vieira <romulo.vieira777@gmail.com>
2022-12-13 20:50:42 -0300

    IAC (Infrastructure As Code) Terraform

--- Section_02_IAC_Infrastructure_as_code_terraform/TF/EC2/main.tf
+++ Section_02_IAC_Infrastructure_as_code_terraform/TF/EC2/main.tf
@@ -8,3 +8,11 @@ resource "aws_instance" "EC2" {
     ami = "ami-0c2b8ca1dad447f8a"
     instance_type = "t2.micro"
 }
+
+resource "aws_eip" "MyEIP" {
+    instance = aws_instance.EC2.id
+}
+
+output "myOutEIP" {
+    value = aws_eip.MyEIP.public_ip  
+}
