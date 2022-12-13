1d2c2f0cfed7bc0af4c2cd699f44108e82867001
Romulo Vieira <romulo.vieira777@gmail.com>
2022-12-13 20:59:46 -0300

    IAC (Infrastructure As Code) Terraform

--- Section_02_IAC_Infrastructure_as_code_terraform/TF/EC2/main.tf
+++ Section_02_IAC_Infrastructure_as_code_terraform/TF/EC2/main.tf
@@ -7,10 +7,31 @@ provider "aws" {
 resource "aws_instance" "EC2" {
     ami = "ami-0c2b8ca1dad447f8a"
     instance_type = "t2.micro"
+    security_groups = [aws_security_group.mySG.name]
 }
 
 resource "aws_eip" "MyEIP" {
     instance = aws_instance.EC2.id
+}
+
+resource "aws_security_group" "mySG" {
+    name = "Allow HTTPS"
+
+    ingress {
+        description = "Allow HTTPS from any where"
+        from_port = 443
+        to_port = 443
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress = {
+        description = "Allow HTTPS from any where"
+        from_port = 443
+        to_port = 443
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
 }
 
 output "myOutEIP" {
