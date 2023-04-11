f25cbb40476488d687d204737d776bcfabe20e65
Romulo Vieira <romulo.vieira777@gmail.com>
2023-04-11 20:44:34 -0300

    IAC (Infrastructure As Code) Terraform

---
+++ Section_02_IAC_Infrastructure_as_code_terraform/TF/module/EC2/ec2.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+    region = "us-east-1"
+    access_key = ""
+    secret_key = ""  
+}
+
+# Create a new EC2 instance
+resource "aws_insta" "myEC2" {
+    ami = ""
+    instance_type = "t2.micro"
+}
